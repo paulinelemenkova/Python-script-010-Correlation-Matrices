b9b0055d32ca60d1fa0025f675a133a2045b0975
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-21 16:08:49 +0800

    Add files via upload

--- Script-010-CorrMatr-Kendall.py
+++ Script-010-CorrMatr-Kendall.py
@@ -3,6 +3,7 @@
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pylab as pylab
 from matplotlib import pyplot as plt
 import seaborn as sb
 
@@ -10,16 +11,18 @@ os.chdir('/Users/pauline/Documents/Python')
 sb.set(style='white')
 sb.set_context('paper')
 
+params = {'figure.figsize': (10, 10),
+        'legend.fontsize': 14,
+        'font.family': 'Times'
+        }
+pylab.rcParams.update(params)
+
 df = pd.read_csv("Tab-Morph.csv")
-
-# compute correlation matrix
 corr = df.corr(method='kendall')
-
-# generate a custom diverging colormap
 cmap="RdYlBu"
 
 # plotting heatmap
-f, ax = plt.subplots(figsize=(11, 9))
+f, ax = plt.subplots()
 sb.heatmap(corr, cmap=cmap, center=0,
            vmin=-1.1, vmax=1.1,
            annot=True, annot_kws={"size":8},
@@ -29,8 +32,8 @@ sb.heatmap(corr, cmap=cmap, center=0,
 plt.title('Mariana Trench: Kendall correlation for the environmental factors',
           fontsize=13, fontfamily='serif')
 plt.subplots_adjust(bottom=0.15,top=0.85, right=0.90, left=0.10)
-plt.xticks(rotation=30, size=9)
-plt.yticks(rotation=0, size=9)
+plt.xticks(rotation=30)
+plt.yticks()
 
 # visualizing and saving figure
 plt.tight_layout()
